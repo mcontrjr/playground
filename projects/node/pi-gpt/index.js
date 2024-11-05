910de1ba1aa05c3ae1c20e14a1b2eea059f83027
Martin <m.contr.jr@gmail.com>
2024-11-05 00:16:15 -0800

    Added weather and cool routes

--- projects/node/pi-gpt/index.js
+++ projects/node/pi-gpt/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
-const os = require('os'); // Use the os module to get system information
+const os = require('os');
+const axios = require('axios');
+const { title } = require('process');
+const { webcrypto } = require('crypto');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -31,11 +34,34 @@ app.get('/info', (req, res) => {
     res.render('info', { title: 'Server Information', serverSpecs });
 });
 
+// Route for a cool pic
+app.get('/cool', (req, res) => {
+    res.render('cool', { title: 'Told you, just a cool pic' });
+})
+
 // Route for the guessing game
 app.get('/guess', (req, res) => {
     // Random number between 1 and 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     res.render('guess', { randomNumber });
+});
+
+// Function to get weather data
+async function getWeather(city) {
+    const WEATHER_TOKEN = process.env.WEATHER_TOKEN
+    const url = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_TOKEN}&q=${city}&aqi=no`;
+
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error.response.data);
+    }
+}
+
+app.get('/weather', async (req, res) => {;
+    const myWeather = await getWeather('95128')
+    res.render('weather', { title: 'MyWeather', myWeather })
 });
 
 // Start the server
