f2973e709c3f358b0212b71cb51a92d7dfa5d109
mcontr <m.contr.jr@gmail.com>
2025-05-30 00:23:27 -0700

    redesign v2!

--- projects/mcontr_dev/components/footer.jsx
+++ projects/mcontr_dev/components/footer.jsx
@@ -1,15 +1,12 @@
-import reactLogo from '../src/assets/react.svg'
 import '../styles/stock.css'
 
 export default function Footer() {
     return (
-        <footer>
-            <p>&copy; 2024 MyPy App. All Rights Reserved.</p>
-            <p>Powered by
-                <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="my-footer-logo react" alt="React logo"/>
-                </a>
-            </p>
+        <footer className="my-footer">
+            <div className="my-container">
+                <p>&copy; 2024 MyPy App. All Rights Reserved.</p>
+                <p>Powered by <a href="https://react.dev" target="_blank" rel="noopener noreferrer">React</a></p>
+            </div>
         </footer>
     )
 }
