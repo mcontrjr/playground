5fe839550760d1792489f820e63c54a5e182d97d
Martin Contreras <m.contr.jr@gmail.com>
2025-12-14 18:03:06 -0800

    nvim

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = false, -- Disable scrolling animations
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
